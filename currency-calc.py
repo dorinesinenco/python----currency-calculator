6d61c2bf92b6534c12a7668a0403cdb0302c6bb0
dorinesinenco <dorin.e.art@gmail.com>
2022-03-23 17:29:47 +0200

    CLass example finished

---
+++ currency-calc.py
@@ -0,0 +1,21 @@
+# Currency Calculator
+# INS:
+#  * EUR/USD amount of money  100USD, 60EUR
+# OUT:
+#  * euquivalent in MDL
+
+k_eur_2_mdl = 20.00
+k_usd_2_mdl = 18.00
+
+
+str_amount = input("Enter money amount (USD/EUR): ")
+
+
+
+amount   = int( str_amount[:-3] )
+currency = str_amount[-3:] 
+
+amount_mdl = amount * k_usd_2_mdl
+
+print(amount, currency, " => ", amount_mdl, "MDL")
+  
